20806a459bbf5607dccb7dd04d9b158283b9c42f
eagles <secret>
2019-08-16 15:47:38 +0800

    开始的版本界面

---
+++ welcome.c
@@ -0,0 +1,12 @@
+#include"headers.h"
+#include"welcome.h"
+
+void welcome()
+{
+	printf("%s\n",LINE);
+	printf("%s\n",BLANK);
+	printf("%s\n",INFO);
+	printf("%s\n",BLANK);
+	printf("%s\n",LINE);
+}
+// printf("|%-15s|/n",insertTime); 
---
+++ welcome.h
@@ -0,0 +1,12 @@
+#ifndef WELCOM_H
+#define WELCOM_H
+
+#ifndef VERSION_1_0
+#define LINE  "**********************************************"
+#define BLANK "*                                            *"
+#define INFO  "*    Version: 3.0  Author: eagles            *"
+#endif
+
+void welcome();
+
+#endif
