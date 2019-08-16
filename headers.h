7607b2b096abcdf95a20a4405711ba8544275933
eagles <secret>
2019-08-16 15:49:05 +0800

    C头文件

---
+++ headers.h
@@ -0,0 +1,17 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/time.h>
+#include<signal.h>
+#include<sys/wait.h>
+#include<math.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<dirent.h>
+#include<sys/types.h>
+#include<fcntl.h>
+
+#endif
