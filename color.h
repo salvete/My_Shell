1485d4cbceaa3b2d58657b54c469936f2f77590f
eagles <secret>
2019-08-16 19:08:25 +0800

    备选颜色

---
+++ color.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define KNRM "\x1B[0m"
+#define KRED "\x1B[31m"
+#define KGRN "\x1B[32m"
+#define KYEL "\x1B[33m"
+#define KBLU "\x1B[34m"
+#define KMAG "\x1B[35m"
+#define KCYN "\x1B[36m"
+#define KWHT "\x1B[37m"
+#define BLUE "\033[0;32;34m"
+
+#endif
